94f3a82d8fc4ed2f11e126afae5bfbd06f575f7a
Sanjay-Pr <sanjayjaiswal9818621124@gmail.com>
2026-06-17 10:39:07 +0530

    ternary completed

--- ternary_operator.java
+++ ternary_operator.java
@@ -1,5 +1,15 @@
 class ternary_operator {
     public static void main(String [] args){
-        
+        //greatest among two numbers
+        int a =10;
+        int b=15;
+        int result = a>b ? a : b;
+        System.out.println("greatest among two is:" + result);
+
+        //greatest among 3
+        int c= 12;
+        result = a>b && a>c ? a : b>c ? b: c;
+        System.out.println("Greatest among three is: "+result);
+
     }
 }
