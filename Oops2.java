f57e662ada2bdd6d4d0ddfb2f9cec181f7271ddd
Sanjay-Pr <sanjayjaiswal9818621124@gmail.com>
2026-07-05 23:26:37 +0530

    Function Overloading

--- Oops2.java
+++ Oops2.java
@@ -1,7 +1,10 @@
 class Abc{
+    String penColor;    //Instance Variable
     public static void playMusic(){                     //this method can be called without using object
         System.out.println("Music is being played....");
     }
+
+    //Method  Overloading
     public String pen(int cost){
         if(cost<=10){
             return "Not Enough Moaney";
@@ -11,6 +14,17 @@ class Abc{
         }
 
     }
+    public String pen(int cost, String color)
+    {
+        if(cost>=20 && color==""){
+            color=penColor;
+            return (color+"Pen");
+        }
+        if(cost>=20 && color!=""){
+            return (color+"Pen");
+        }
+        return "Pencil No Pen";
+    }
 }
 
 public class Oops2 {
@@ -18,6 +32,14 @@ public class Oops2 {
         int x=10;
         Abc.playMusic();
         Abc obj = new Abc();
+        Abc obj1 =new Abc();
         System.out.println(obj.pen(x));
+        System.out.println(obj.pen(15, "Green"));
+
+        obj1.penColor="Black";
+        obj.penColor="Red";
+
+        System.out.println(obj1.pen(25, ""));
+        System.out.println(obj.pen(30, ""));      
     }
 }
