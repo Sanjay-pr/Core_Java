6952244c275947804be5c1739995f0e43cf02363
Sanjay-Pr <sanjayjaiswal9818621124@gmail.com>
2026-07-07 23:30:31 +0530

    Construcor and Static keyword

--- Encapsulation.java
+++ Encapsulation.java
@@ -2,6 +2,20 @@ class Teacher{
     private String name;
     private int age;
     private String education;
+    
+    static String school;
+
+    public Teacher(){               // default Constructor
+        name="Sanjay";
+        age=24;
+        education="Graduate";
+    }
+
+    public Teacher(int age, String name, String education){         //parameterized Constructor
+        this.age=age;
+        this.education=education;
+        this.name=name;
+    }
 
     public void setName(String name){
         this.name=name;
@@ -24,12 +38,23 @@ class Teacher{
         return education;
     }
 
+    public void Show(){
+        System.out.println(school);
+        System.out.println(name+ " : " + age+ " : " + education);
+
+    }
+
 
 }
 
 public class Encapsulation {
     public static void main(String[] args) {
         Teacher obj = new Teacher();
+        System.out.println("Default Constructor Output");
+        System.out.println(obj.getName() + " : " + obj.getAge() + " : " + obj.getEducation());
+        Teacher obj1= new Teacher(24,"Raj","Graduate");
+        System.out.println("Parameterized Constructor Output");
+        System.out.println(obj1.getName() + " : " + obj1.getAge() + " : " + obj1.getEducation());
         
         String name ="Urmi";
         String education ="Masters";
@@ -40,6 +65,11 @@ public class Encapsulation {
         obj.setEducation(education);
 
         System.out.println(obj.getName() + " : " + obj.getAge() + " : " + obj.getEducation());
+
+
+        Teacher.school="Holy Light";
+        System.out.println("Displaying static keyword");
+        obj.Show();
 
     }
 }
